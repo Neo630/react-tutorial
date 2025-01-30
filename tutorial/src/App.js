6f22b141029d6b3b33c7329f7f780518f85b10fc
Neo630 <jmsantana2004@gmail.com>
2025-01-30 18:57:08 -0500

    tut3

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,25 +1,18 @@
 // Following tutorial: https://www.youtube.com/watch?v=RVFAyFWO4go&t=759s
 
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from './Footer'
 
 function App() {
-  const handleNameChange = () => {
-    const names = ['Bob', 'Kevin', 'Dave'];
-    const int = Math.floor(Math.random() * 3);
-    return names[int];
-  }
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {handleNameChange()}!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <Content />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
