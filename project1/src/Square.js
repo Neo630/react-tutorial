dbc28c6a1144f51b8e85f96a797cd746a5d857bd
Neo630 <jmsantana2004@gmail.com>
2025-02-12 13:36:22 -0500

    project changes

--- project1/src/Square.js
+++ project1/src/Square.js
@@ -1,7 +1,13 @@
-const Square = ({ colorValue }) => {
+const Square = ({ colorValue, hexValue, isDarkText }) => {
     return (
-        <section className='square' style={{ backgroundColor: colorValue }}>
+        <section
+            className='square'
+            style={{
+                backgroundColor: colorValue,
+                color: isDarkText ? "black" : "white",
+            }}>
             <p>{colorValue ? colorValue : "Empty Value"}</p>
+            <p>{hexValue ? hexValue : null}</p>
         </section>
     );
 };
