4b256bcdb4bf8512b768fa2cacf2d70dd7e781ef
Neo630 <jmsantana2004@gmail.com>
2025-02-12 13:26:56 -0500

    project1

---
+++ project1/src/Square.js
@@ -0,0 +1,9 @@
+const Square = ({ colorValue }) => {
+    return (
+        <section className='square' style={{ backgroundColor: colorValue }}>
+            <p>{colorValue ? colorValue : "Empty Value"}</p>
+        </section>
+    );
+};
+
+export default Square;
